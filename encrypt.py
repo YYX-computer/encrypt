f815e2324685b177456ffa5b0559c203e6d963bf
YYX-computer <59608785+YYX-computer@users.noreply.github.com>
2020-06-11 19:08:46 +0800

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -14,7 +14,7 @@ class encrypt():
 		self.HV = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'A':10,'B':11,'C':12,'D':13,'E':14,'F':15,'a':10,'b':11,'c':12,'d':13,'e':14,'f':15,'x':0,'d':0}
 		print('your string is:%s'%_get_hex(self.string))
 	def __str__(self):
-		return self.Encrypt()
+		return '0x' + ''.join(self.Encrypt().split('0x'))
 	def _hexadd(self,*arg):
 		hexNum = ''
 		sumNum = 0
